f16d2f9fc9f49ee8a0bd1d584fa26653d5ba0a09
selva-mern12 <selva.mern12@gmail.com>
2025-03-29 15:06:53 +0530

    responsive header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from '@vercel/analytics/react';
-import Header from './components/Header';
+import {Header, Footer} from './components/Header';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -19,6 +19,7 @@ const App = () => {
       <Skills />
       <Resume />
       <Contact />
+      {window.innerWidth < 768 && <Footer />}
       <Analytics />
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { FaBars, FaTimes, FaProjectDiagram, FaCode, FaFileAlt, FaEnvelope } from 'react-icons/fa';
+import { FaProjectDiagram, FaCode, FaFileAlt, FaEnvelope } from 'react-icons/fa';
 import { GiEarthAmerica } from "react-icons/gi";
 import './index.css';
 
-const Header = () => {
+export const Header = () => {
   const sections = useMemo(() => ["overview", "projects", "skills", "resume", "contact"], []);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeTab, setActiveTab] = useState("overview");
 
   useEffect(() => {
@@ -28,7 +27,6 @@ const Header = () => {
   }, [sections]);
 
   const handleClick = (id) => {
-    setIsMenuOpen(false); 
     document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
   };
 
@@ -40,12 +38,65 @@ const Header = () => {
               }}>
         <h1 className="header-title">Selva's Briefview</h1>
         </a>
-      
-      <div className="hamburger-icon" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-        {isMenuOpen ? <FaTimes /> : <FaBars />}
-      </div>
 
-      <ul className={`hamburger-menu ${isMenuOpen ? "active" : ""}`}>
+      {window.innerWidth > 768 && <ul className="hamburger-menu">
+        {sections.map((id) => {
+          const icons = {
+            overview: <GiEarthAmerica />,
+            projects: <FaProjectDiagram />,
+            skills: <FaCode />,
+            resume: <FaFileAlt />,
+            contact: <FaEnvelope />,
+          };
+          return (
+            <li key={id} className={`menu-item ${activeTab === id ? "active-tab" : ""}`}>
+              <a href={`#${id}`} className="menu-link" onClick={(e) => {
+                e.preventDefault();
+                handleClick(id);
+              }}>
+                <div className="nav-icon">{icons[id]}</div>
+                <span className="menu-text">{id.charAt(0).toUpperCase() + id.slice(1)}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>}
+    </div>
+  );
+};
+
+
+export const Footer = () => {
+  const sections = useMemo(() => ["overview", "projects", "skills", "resume", "contact"], []);
+  const [activeTab, setActiveTab] = useState("overview");
+  console.log(activeTab);
+  useEffect(() => {
+    setActiveTab(null)
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const visibleSection = entries.find((entry) => entry.isIntersecting);
+        if (visibleSection) {
+          setActiveTab(visibleSection.target.id);
+        }
+      },
+      { threshold: 0.4 } 
+    );
+
+    sections.forEach((id) => {
+      const section = document.getElementById(id);
+      if (section) observer.observe(section);
+    });
+
+    return () => observer.disconnect();
+  }, [sections]);
+
+  const handleClick = (id) => {
+    document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className="footer-container">
+      <ul className="hamburger-menu">
         {sections.map((id) => {
           const icons = {
             overview: <GiEarthAmerica />,
@@ -70,5 +121,3 @@ const Header = () => {
     </div>
   );
 };
-
-export default Header;
